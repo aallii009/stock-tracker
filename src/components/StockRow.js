d8e977699321deaa8f0b4edf4a9f55e6b5cecd64
aallii009 <alimohamad5542@gmail.com>
2021-02-11 18:07:01 -0800

    added basic setup for the app

---
+++ src/components/StockRow.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class StockRow extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: {}
+    }
+  }
+
+  componentDidMount() {
+    // query the API
+    this.setState( {
+      data: {
+        price: 5,
+        date: '12-12-12',
+        time: '09:45'
+      }
+    })
+  }
+
+  render() {
+    return (
+
+    <tr>
+      <td>{this.props.ticker}</td>
+      <td>{this.state.data.price}</td>
+      <td>{this.state.data.date}</td>
+      <td>{this.state.data.time}</td>
+    </tr>
+  )
+  }
+}
+
+export default StockRow;
