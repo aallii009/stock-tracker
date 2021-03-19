926007adb37207db95c46d521f9383e7567b44a5
Ali Mohamad <55715694+aallii009@users.noreply.github.com>
2021-03-18 17:44:11 -0700

    added stockList

--- src/components/StockRow.js
+++ src/components/StockRow.js
@@ -2,24 +2,49 @@ import React, { Component } from 'react';
 import  { iex } from '../config/iex.js';
 import { stock } from '../resources/stock.js';
 
-const changeStyle = {
-  color:'#4caf50',
-  fontSize: '0.7rem',
-  marginLeft: 5
-}
+
 class StockRow extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      data: {}
+      price: null,
+      date: null,
+      time:null,
+      dollar_change: null,
+      percent_change: null
+
+
     }
   }
 
+  changeStyle() {
+
+    return {
+     color: (this.state.dollar_change > 0) ? '#4caf50' :'#e53935' ,
+     fontSize: '0.7rem',
+     marginLeft: 5
+
+    }
+
+  }
   applyData(data) {
+
+    const formattedPrice = (data.price == undefined ) ? null: data.price.toFixed(2)
     this.setState({
-     data: data
+    price: formattedPrice,
+    date: data.date,
+    time: data.time
   })
+    stock.getYesterdaysClose(this.props.ticker, this.props.lastTradingDate, (yesterday) => {
+
+      const dollar_change = (data.price - yesterday.price).toFixed(2);
+      const percent_change = (100 * dollar_change / yesterday.price).toFixed(2);
+      this.setState({
+        dollar_change: `${dollar_change}`,
+        percent_change: `(${percent_change}%)`
+      })
+    })
   }
 
   componentDidMount() {
@@ -27,12 +52,18 @@ class StockRow extends Component {
     stock.latestPrice(this.props.ticker, this.applyData.bind(this))
   }
 
+  componentDidUpdate() {
+    console.log(this.props.lastTradingDate)
+  }
+
   render() {
     return (
       <li className = "list-group-item">
-       <b>{this.props.ticker}</b> ${this.state.data.price}
-       <span className = "change" style = {changeStyle}>
-        +12.34(5.3%)
+       <b>{this.props.ticker}</b> ${this.state.price}
+       <span className = "change" style = {this.changeStyle()}>
+       {this.state.dollar_change}
+       {this.state.percent_change}
+
        </span>
     </li>
 
--- src/resources/stock.js
+++ src/resources/stock.js
@@ -22,6 +22,38 @@ formatPriceData: (data) => {
     formattedData.time = stockData.label
     return formattedData
 
+},
+
+getYesterdaysClose: (ticker, lastTradingDate, callback) => {
+  //stock.getLastTradingDate(date).then((data) => {
+  if (lastTradingDate != "") {
+  fetch(stock.yesterdaysCloseURL(ticker, lastTradingDate))
+  .then((response) => response.json())
+  .then((data) => callback(stock.formatPriceData(data)))
 }
+
+//  })
+
+},
+
+getLastTradingDate: () => {
+  var today = new Date().toISOString().split('T')[0].replace(/-/g, '')
+  const url = `${iex.base_url}/ref-data/us/dates/trade/last/1/${today}?token=${iex.api_token}`
+
+  return fetch(url).then((res) => res.json());
+},
+
+yesterdaysCloseURL: (ticker, lastTradingDate) => {
+
+ //var lastTradingDate = stock.formatDate(date);
+
+
+ return  `${iex.base_url}/stock/${ticker}/intraday-prices?chartLast=1&exactDate=${lastTradingDate}&token=${iex.api_token}`
+},
+
+formatDate: (date) => {
+  return new Date(date).toISOString().split('T')[0].replace(/-/g, '')
+}
+
 
 }
